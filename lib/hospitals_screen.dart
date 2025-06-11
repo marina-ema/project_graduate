0a7018260d0998718c7183f9ce55dcab7f38f920
marina-ema <144705875+marina-ema@users.noreply.github.com>
2025-06-11 05:49:24 +0300

    Update hospitals_screen.dart

--- lib/hospitals_screen.dart
+++ lib/hospitals_screen.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 class HospitalsScreen extends StatelessWidget {
   final String governorate;
-
   const HospitalsScreen({Key? key, required this.governorate})
       : super(key: key);
-
   Future<void> _launchMapUrl(String url) async {
     final Uri uri = Uri.parse(url);
     if (await canLaunchUrl(uri)) {
@@ -22,7 +21,7 @@ class HospitalsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 11, 170, 143),
-        title: Text('Hospitals in $governorate'),
+        title: Text('hospitals_in'.tr(args: [governorate])),
       ),
       body: StreamBuilder<QuerySnapshot>(
         stream: FirebaseFirestore.instance
@@ -34,20 +33,16 @@ class HospitalsScreen extends StatelessWidget {
             return const Center(child: CircularProgressIndicator());
           }
           if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-            return const Center(child: Text('No hospitals found.'));
+            return Center(child: Text('no_hospitals_found'.tr()));
           }
-
           final docs = snapshot.data!.docs;
-
           return ListView.builder(
             itemCount: docs.length,
             itemBuilder: (context, index) {
               final data = docs[index].data() as Map<String, dynamic>;
-
-              final name = data['name'] ?? 'No name';
-              final address = data['address'] ?? 'No address';
+              final name = data['name'] ?? 'no_name'.tr();
+              final address = data['address'] ?? 'no_address'.tr();
               final mapUrl = data['map_url'];
-
               return ListTile(
                 title: Text(name),
                 subtitle: Text(address),
@@ -57,7 +52,7 @@ class HospitalsScreen extends StatelessWidget {
                     _launchMapUrl(mapUrl);
                   } else {
                     ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Map link not available.')),
+                      SnackBar(content: Text('map_not_available'.tr())),
                     );
                   }
                 },
